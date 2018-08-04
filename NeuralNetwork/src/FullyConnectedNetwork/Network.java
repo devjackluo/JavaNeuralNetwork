97005936a61b8f4f34f2cffa4ab9a7a0e61c7c75
Jack Luo <jackluo94@hotmail.com>
2018-08-04 01:15:42 -0700

    basic feed forward

--- NeuralNetwork/src/FullyConnectedNetwork/Network.java
+++ NeuralNetwork/src/FullyConnectedNetwork/Network.java
@@ -1,42 +1,95 @@
 package FullyConnectedNetwork;
 
+import java.util.Arrays;
+
+import static FullyConnectedNetwork.NetworkTools.createRandomArray;
+import static FullyConnectedNetwork.NetworkTools.createRandomMultiArray;
+
 public class Network {
 
     private double[][] output;
     private double[][][] weights;
     private double[][] bias;
 
-    public final int[] NETWORK_LAYER_SIZE;
+    public final int[] NETWORK_LAYER_SIZES;
     public final int INPUT_SIZE;
     public final int OUTPUT_SIZE;
     public final int NETWORK_SIZE;
 
     public Network(int... Network_Layer_Size){
-        this.NETWORK_LAYER_SIZE = Network_Layer_Size;
-        this.NETWORK_SIZE = NETWORK_LAYER_SIZE.length;
-        this.INPUT_SIZE = NETWORK_LAYER_SIZE[0];
-        this.OUTPUT_SIZE = NETWORK_LAYER_SIZE[NETWORK_SIZE-1];
+        this.NETWORK_LAYER_SIZES = Network_Layer_Size;
+        this.NETWORK_SIZE = NETWORK_LAYER_SIZES.length;
+        this.INPUT_SIZE = NETWORK_LAYER_SIZES[0];
+        this.OUTPUT_SIZE = NETWORK_LAYER_SIZES[NETWORK_SIZE-1];
 
         //first dimension keeps track of which layer it is on
         //second dimension keeps track of which node of the layer
         this.output = new double[NETWORK_SIZE][];
         this.bias = new double[NETWORK_SIZE][];
         //weights get a special third dimension to keep track of node from previous layer
+        //where 2nd dimension is previous and 3rd is current node (previous --weight--> current) but doesn't matter
+        // currently grabbing weights (current <--weight-- previous)
         this.weights = new double[NETWORK_SIZE][][];
 
         for(int i = 0; i < NETWORK_SIZE; i++){
-            this.output[i] = new double[NETWORK_LAYER_SIZE[i]];
-            this.bias[i] = new double[NETWORK_LAYER_SIZE[i]];
 
+            this.output[i] = new double[NETWORK_LAYER_SIZES[i]];
+            //this.bias[i] = new double[NETWORK_LAYER_SIZES[i]];
+            this.bias[i] = createRandomArray(NETWORK_LAYER_SIZES[i], 0.0, 1.0);
+
+
+            //the first layer doesn't have a weight because no previous layer
             if(i > 0){
-                weights[i] = new double[NETWORK_LAYER_SIZE[i]][NETWORK_LAYER_SIZE[i-1]];
+                //this.weights[i] = new double[NETWORK_LAYER_SIZES[i]][NETWORK_LAYER_SIZES[i-1]];
+                this.weights[i] = createRandomMultiArray(NETWORK_LAYER_SIZES[i],NETWORK_LAYER_SIZES[i-1], -0.99, 0.99);
             }
         }
 
     }
 
+    public double[] calculate(double... input){
+        if(input.length != this.INPUT_SIZE){
+            return null;
+        }
+
+        //set the first layer to be the input we gave to network
+        this.output[0] = input;
+
+        //for all hidden layers
+        for(int layer = 1; layer < NETWORK_SIZE; layer++){
+            //for all neurons(nodes) in those layers
+            for(int neuron = 0; neuron < NETWORK_LAYER_SIZES[layer]; neuron++){
+
+                //could put this after calculating weights but then bias won't have much impact
+                double sum = bias[layer][neuron];
+
+                //take the value every previous neuron and multiple it by the weight going to towards this neuron
+                //add it to sum
+                for(int prevNeuron = 0; prevNeuron < NETWORK_LAYER_SIZES[layer-1]; prevNeuron++){
+                    sum += output[layer-1][prevNeuron] * weights[layer][neuron][prevNeuron];
+                }
+
+                //sigmoid the sum (0.0000- to 1.0000-)
+                output[layer][neuron] = sigmoid(sum);
+
+            }
+        }
+
+        //return the last layer (the predicted value / guess)
+        return output[NETWORK_SIZE-1];
+    }
+
+
+    private double sigmoid(double x){
+        return (1d / (1 + Math.exp(-x)));
+    }
+
     public static void main(String[] args){
         Network net = new Network(4,3,2,3,4);
+        double[] results = net.calculate(0.2,0.9,0.3,0.4);
+
+        System.out.println(Arrays.toString(results));
+        System.out.println("Pause");
     }
 
 }
---
+++ NeuralNetwork/src/FullyConnectedNetwork/NetworkTools.java
@@ -0,0 +1,166 @@
+package FullyConnectedNetwork;
+
+public class NetworkTools {
+
+
+    public static double[] createArray(int size, double init_value){
+        if(size < 1){
+            return null;
+        }
+        double[] ar = new double[size];
+        for(int i= 0; i < ar.length; i++){
+            ar[i] = init_value;
+        }
+        return ar;
+    }
+
+
+    public static double[] createRandomArray(int size, double min, double max){
+        if(size < 1){
+            return null;
+        }
+        double[] ar = new double[size];
+        for(int i= 0; i < ar.length; i++){
+            ar[i] = randomValue(min, max);
+        }
+        return ar;
+    }
+
+    public static double[][] createRandomMultiArray(int sizeX, int sizeY, double min, double max){
+        if(sizeX < 1 && sizeY < 1){
+            return null;
+        }
+        double[][] ar = new double[sizeX][sizeY];
+        for(int i= 0; i < ar.length; i++){
+            ar[i] = createRandomArray(sizeY, min, max);
+        }
+        return ar;
+    }
+
+    public static double randomValue(double min, double max){
+        return (Math.random() * (max-min)) + min;
+    }
+
+    public static Integer[] randomIntArray(int min, int max, int size){
+        min--;
+        if(size > (max-min)){
+            return null;
+        }
+        Integer[] values = new Integer[size];
+        for(int i = 0; i < size; i++){
+            int n = (int)(Math.random()*(max-min+1) + min);
+            while(containsValue(values, n)){
+                n = (int)(Math.random()*(max-min+1) + min);
+            }
+            values[i] = n;
+        }
+        return values;
+    }
+
+    public static <T extends Comparable<T>> boolean containsValue(T[] arr, T value){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != null){
+                if(value.compareTo(arr[i]) == 0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int indexOfHighestValue(double[] values){
+        int index = 0;
+        for(int i = 1; i < values.length; i++){
+            if(values[i] > values[index]){
+                index = i;
+            }
+        }
+        return index;
+    }
+
+
+
+    /*
+    public static double[] createArray(int size, double init_value){
+        if(size < 1){
+            return null;
+        }
+        double[] ar = new double[size];
+        for(int i = 0; i < size; i++){
+            ar[i] = init_value;
+        }
+        return ar;
+    }
+
+    public static double[] createRandomArray(int size, double lower_bound, double upper_bound){
+        if(size < 1){
+            return null;
+        }
+        double[] ar = new double[size];
+        for(int i = 0; i < size; i++){
+            ar[i] = randomValue(lower_bound,upper_bound);
+        }
+        return ar;
+    }
+
+    public static double[][] createRandomMultiArray(int sizeX, int sizeY, double lower_bound, double upper_bound){
+        if(sizeX < 1 || sizeY < 1){
+            return null;
+        }
+        double[][] ar = new double[sizeX][sizeY];
+        for(int i = 0; i < sizeX; i++){
+            ar[i] = createRandomArray(sizeY, lower_bound, upper_bound);
+        }
+        return ar;
+    }
+
+    public static double randomValue(double lower_bound, double upper_bound){
+        return Math.random()*(upper_bound-lower_bound) + lower_bound;
+    }
+
+    public static Integer[] randomValues(int lowerBound, int upperBound, int amount) {
+
+        lowerBound --;
+
+        if(amount > (upperBound-lowerBound)){
+            return null;
+        }
+
+        Integer[] values = new Integer[amount];
+        for(int i = 0; i< amount; i++){
+            int n = (int)(Math.random() * (upperBound-lowerBound+1) + lowerBound);
+            while(containsValue(values, n)){
+                n = (int)(Math.random() * (upperBound-lowerBound+1) + lowerBound);
+            }
+            values[i] = n;
+        }
+        return values;
+    }
+
+    public static <T extends Comparable<T>> boolean containsValue(T[] ar, T value){
+        for(int i = 0; i < ar.length; i++){
+            if(ar[i] != null){
+                if(value.compareTo(ar[i]) == 0){
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+    public static int indexOfHighestValue(double[] values){
+        int index = 0;
+        for(int i = 1; i < values.length; i++){
+            if(values[i] > values[index]){
+                index = i;
+            }
+        }
+        return index;
+    }*/
+
+
+
+
+
+}
